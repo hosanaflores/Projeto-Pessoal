2a0f80abb9c45adb66a3aee1ed134755e16fc6a7
hosanaflores <hosana.flores@sptech.school>
2024-07-17 16:25:06 -0300

    Área de Restaurantes responsivo

--- site/public/js/restaurante-modal.js
+++ site/public/js/restaurante-modal.js
@@ -1,11 +1,10 @@
-
-/*Abrir cardápio*/
-function verCardapio(){
-        document.getElementById('meuModal').style.display = 'flex';
+function verCardapio() {
+  document.getElementById("meuModal").style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
 }
 
-/*Fechar cardápio*/ 
-function fecharCardapio(){
-    document.getElementById('meuModal').style.display = 'none';
-    meuModal.innerHTML = ``; /* para limpar*/
+function fecharCardapio() {
+  document.getElementById("meuModal").style.display = "none";
+  meuModal.innerHTML = ``;
+  document.querySelector("body").style.overflow = "scroll";
 }
